4853b2cf8fd6de73b6c33eff595bc72e7703003b
Gonzalo Diaz <gonzalo@bytelion.com>
2015-02-22 17:04:33 -0300

    reorganize code

--- run_analysis.R
+++ run_analysis.R
@@ -1,5 +1,5 @@
 run_analysis <- function() {
-    # Step 0 - Install dependencies and obtain the DataSet
+    # Install dependencies and obtain the DataSet
     loadDependencies()
     cat("Obtaining the data set\n")
     if(!file.exists("./data")){dir.create("./data")}
@@ -27,38 +27,42 @@ run_analysis <- function() {
     x_set <- rbind(train_x, test_x)
     y_set <- rbind(train_y, test_y)
     
+    cat("Merge completed.\n")
     # Assign Column Names
+    cat("Assigning column names...\n")
     features <- read.table("./data/UCI HAR Dataset/features.txt")
     names(x_set) <- features$V2
     names(y_set) <- "Activity"
     names(subject_set) <- "Subject"
-    
-    cat("Merge completed.\n")
    
     # Extract Measurements of Mean and Standard deviation
+    cat("Extracting measurements of mean and standard deviation...\n")
     mean_cols <- matchcols(x_set, with=c("mean"), without=c("meanFreq"))
     std_cols  <- matchcols(x_set, with=c("std"))
     new_cols <- c(mean_cols, std_cols)
     x_sub_set <- subset(x_set, select=new_cols)
     
-    # Use descriptive activity names to name the activities
-    # Map Activity Value
+    # Use descriptive activity names to map the Activity value
+    cat("Mapping Activity values...\n")
     activity_labels <- read.table("./data/UCI HAR Dataset/activity_labels.txt")
     activity_int_as_char <- as.character(activity_labels$V1)
     activity_factor_as_char <- as.character(activity_labels$V2)
     y_set$Activity <- mapvalues(as.character(y_set$Activity), from = activity_int_as_char, to = activity_factor_as_char)
     
-    # Create one DataSet
+    # Create DataSet
+    cat("Creating DataSet and writing it to file...\n")
     dataSet <- cbind(y_set, subject_set, x_sub_set)
+    write.table(dataSet, file="./data/result_step_4.txt", row.name=FALSE)
     
     # Create and Print a new tidy DataSet
+    cat("Creating DataSet with summarised data and writing it to file...\n")
     dataSet2 <- dataSet %>% group_by(Activity, Subject) %>% summarise_each(funs(mean(., na.rm=TRUE)), 3:ncol(dataSet))
     write.table(dataSet2, file="./data/result_step_5.txt", row.name=FALSE)
     
-    return (dataSet)
+    data.frame(dataSet2)
 }
 
-
+## HELPER Methods
 
 # Method to pull the file from the Web in a more efficient way.
 bdown <- function(url, file){
